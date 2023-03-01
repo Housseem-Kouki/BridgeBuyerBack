d3050e706d3987bbeb6bf9144c8ef6035c91b634
Housseem-Kouki <houssem.kouki@esprit.tn>
2023-03-01 20:10:42 +0100

    config Eureka/Gateway

---
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,35 @@
+package com.example.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+
+    // @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                //lb : load balancer
+                .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .route(r->r.path("/bills/**").uri("lb://BILLING-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+                                                               DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+}
